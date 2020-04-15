3950dbc4143ebb2bd7ba7fd8c451e2f8497fca7e
Abhijeet singh <abhijeetranawat13@gmail.com>
2020-04-15 22:19:26 +0530

    Edited the two function according to the requirements

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix() is a function which has functions to set, get the 
+## values of the matrix whose inverse is to be calculated. It also has functions
+## to retrive the inverse from the cache and push the calculated inverse value 
+## to the cache. The function returns a list , which is a pointer to the functions.
+## The function object can be used to access the functions in makeCacheMatrix()
+## The set function also determines whether the matrix being set is same as the
+## prev matrix using matequal() function. If the matrix being set is same
+## as that whose inverse exists in the cache, then the inverse remains cached.
+
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -19,9 +27,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve() function calculates the inverse of the matrix if the 
+## inverse has not been cached yet.THe inverse of the matrix is calculated
+## if the determinant of the matrix is not zero.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   i <- x$getinverse()
